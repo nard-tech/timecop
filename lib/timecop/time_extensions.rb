64d43702fc4b5d1c83119bae681e3a13da5cef47
Shu Fujita <s.fujita@nard.tech>
2018-05-13 20:19:59 +0900

    Replace ‘case’ clause by ‘if’ in DateTime.#parse_with_mock_date

--- lib/timecop/time_extensions.rb
+++ lib/timecop/time_extensions.rb
@@ -104,12 +104,11 @@ class DateTime #:nodoc:
       return parsed_date unless mocked_time_stack_item
       date_hash = DateTime._parse(*args)
 
-      case
-      when date_hash[:year] && date_hash[:mon]
+      if date_hash[:year] && date_hash[:mon]
         parsed_date
-      when date_hash[:mon] && date_hash[:mday]
+      elsif date_hash[:mon] && date_hash[:mday]
         DateTime.new(mocked_time_stack_item.year, date_hash[:mon], date_hash[:mday])
-      when date_hash[:wday]
+      elsif date_hash[:wday]
         Date.closest_wday(date_hash[:wday]).to_datetime
       else
         parsed_date + mocked_time_stack_item.travel_offset_days

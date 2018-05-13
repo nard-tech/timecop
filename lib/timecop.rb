2d0ca9e40fc22937564800f462e999c4851e60be
Shu Fujita <s.fujita@nard.tech>
2018-05-13 20:29:58 +0900

    Clean style of 'lib/timecop.rb'

--- lib/timecop.rb
+++ lib/timecop.rb
@@ -1,2 +1,2 @@
-require File.join(File.dirname(__FILE__), "timecop", "timecop")
-require File.join(File.dirname(__FILE__), "timecop", "version")
+require File.join(File.dirname(__FILE__), 'timecop', 'timecop')
+require File.join(File.dirname(__FILE__), 'timecop', 'version')

64dfb3953f252347080a9a348b7074c56bf7978d
Shu Fujita <s.fujita@nard.tech>
2018-05-13 22:55:10 +0900

    Add Timecop::DateBaseParser

--- lib/timecop/parsers/date_time_parser.rb
+++ lib/timecop/parsers/date_time_parser.rb
@@ -1,50 +1,13 @@
+require File.join(File.dirname(__FILE__), 'date_base_parser')
+
 class Timecop
-  class DateTimeParser
+  class DateTimeParser < DateBaseParser
     def self.object_class
       ::DateTime
     end
 
-    def self.call(parsed_date, mocked_time_stack_item, *args)
-      new(parsed_date, mocked_time_stack_item, *args).call
-    end
-
-    def initialize(parsed_date, mocked_time_stack_item, *args)
-      @parsed_date = parsed_date
-      @mocked_time_stack_item = mocked_time_stack_item
-      @date_hash = self.class.object_class._parse(*args)
-    end
-
-    def call
-      return parsed_date if year && mon
-      return self.class.object_class.new(mocked_time_stack_item.year, mon, mday) if mon && mday
-      return closest_wday(Date.today, wday) if wday
-      parsed_date + mocked_time_stack_item.travel_offset_days
-    end
-
-    private
-
-    attr_reader :parsed_date, :mocked_time_stack_item, :date_hash
-
-    def year
-      date_hash[:year]
-    end
-
-    def mon
-      date_hash[:mon]
-    end
-
-    def mday
-      date_hash[:mday]
-    end
-
-    def wday
-      date_hash[:wday]
-    end
-
     def closest_wday(today, wday)
-      result = today - today.wday
-      result += 1 until wday == result.wday
-      result.to_datetime
+      super(today, wday).to_datetime
     end
   end
 end
